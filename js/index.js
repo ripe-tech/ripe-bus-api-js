281e6ca09fe0a4ee238cc1b685108c89bca6d903
Margarida Silva <t-ms@platforme.com>
2020-09-22 10:34:31 +0100

    Producer and Consumer base classes

--- js/index.js
+++ js/index.js
@@ -2,3 +2,5 @@ export * from "./base";
 export * from "./consumer";
 export * from "./producer";
 export * from "./kafkaClient";
+export * from "./kafkaConsumer";
+export * from "./kafkaProducer";
--- js/producer.js
+++ js/producer.js
@@ -1,48 +1,15 @@
-import { conf } from "yonius";
-import { CompressionTypes } from "kafkajs";
+import { NotImplementedError } from "yonius";
 
 export class Producer {
-    constructor(kafkaClient) {
-        this.producer = kafkaClient.producer({
-            metadataMaxAge: conf("KAFKA_PRODUCER_METADATA_MAX_AGE", 300000),
-            allowAutoTopicCreation: conf("KAFKA_PRODUCER_AUTO_TOPIC_CREATION", true),
-            transactionTimeout: conf("KAFKA_PRODUCER_TRANSITION_TIMEOUT", 60000)
-        });
+    async connect() {
+        throw new NotImplementedError();
     }
 
-    async connect() {
-        await this.producer.connect();
+    async produce(topic, message) {
+        throw new NotImplementedError();
     }
 
     async disconnect() {
-        await this.producer.disconnect();
-    }
-
-    async run(messages, topic = null) {
-        const convertedMessages = this.__convertMessages(messages);
-
-        await this.producer.send({
-            topic: topic || conf("KAFKA_PRODUCER_TOPIC", ""),
-            acks: conf("KAFKA_PRODUCER_ACKS", 0),
-            timeout: conf("KAFKA_PRODUCER_TIMEOUT", 30000),
-            compression: this._convertCompressionTypes(conf("KAFKA_PRODUCER_COMPRESSION", null)),
-            messages: convertedMessages
-        });
-    }
-
-    _convertCompressionTypes(compression) {
-        if (compression === "gzip") return CompressionTypes.GZIP;
-        return CompressionTypes.None;
-    }
-
-    _convertMessages(messages) {
-        const convertedMessages = [];
-        for (const message of messages) {
-            if (typeof message === "string") convertedMessages.push(message);
-            else convertedMessages.push({ value: JSON.stringify(message) });
-        }
-        return convertedMessages;
+        throw new NotImplementedError();
     }
 }
-
-export default Producer;
