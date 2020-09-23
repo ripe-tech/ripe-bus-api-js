338eaec6326d1ffe3de696ba47210cc9d3300043
Margarida Silva <t-ms@platforme.com>
2020-09-23 10:23:34 +0100

    Refactor configuration variables

--- js/kafka/kafka-client.js
+++ js/kafka/kafka-client.js
@@ -1,34 +1,35 @@
 import { conf } from "yonius";
 import { Kafka, CompressionTypes } from "kafkajs";
 
-export const KafkaClient = (function() {
+export const KafkaClient = (function () {
     let client = null;
     return {
         getInstance: (options = {}) => {
             if (client) return client;
 
-            const hosts =
-                options.hosts === undefined ? conf("KAFKA_HOSTS", "localhost:9092") : options.hosts;
-            const clientId =
-                options.clientId === undefined
-                    ? conf("KAFKA_CLIENT_ID", "ripe-kafka")
-                    : options.clientId;
-            const connectionTimeout =
+            let hosts = conf("KAFKA_HOSTS", "localhost:9092");
+            let clientId = conf("KAFKA_CLIENT_ID", "ripe-kafka");
+            let connectionTimeout = conf("KAFKA_CONNECT_TIMEOUT", 10000);
+            let requestTimeout = conf("KAFKA_REQUEST_TIMEOUT", 30000);
+            let initialRetryTime = conf("KAFKA_INITIAL_RETRY_TIME", 300);
+            let maxRetryTime = conf("KAFKA_MAX_RETRY_TIME", 30000);
+            let retries = conf("KAFKA_RETRIES", 5);
+            let maxInFlightRequests = conf("KAFKA_MAX_INFLIGHT_REQUESTS", null);
+
+            hosts = options.hosts === undefined ? hosts : options.hosts;
+            clientId = options.clientId === undefined ? clientId : options.clientId;
+            connectionTimeout =
                 options.connectionTimeout === undefined
-                    ? conf("KAFKA_CONNECT_TIMEOUT", 10000)
+                    ? connectionTimeout
                     : options.connectionTimeout;
-            const requestTimeout = options.requestTimeout
-                ? conf("KAFKA_REQUEST_TIMEOUT", 30000)
-                : options.requestTimeout;
-            const initialRetryTime = options.initialRetryTime
-                ? conf("KAFKA_INITIAL_RETRY_TIME", 300)
+            requestTimeout = options.requestTimeout ? requestTimeout : options.requestTimeout;
+            initialRetryTime = options.initialRetryTime
+                ? initialRetryTime
                 : options.initialRetryTime;
-            const maxRetryTime = options.maxRetryTime
-                ? conf("KAFKA_MAX_RETRY_TIME", 30000)
-                : options.maxRetryTime;
-            const retries = options.retries ? conf("KAFKA_RETRIES", 5) : options.retries;
-            const maxInFlightRequests = options.maxInFlightRequests
-                ? conf("KAFKA_MAX_INFLIGHT_REQUESTS", null)
+            maxRetryTime = options.maxRetryTime ? maxRetryTime : options.maxRetryTime;
+            retries = options.retries ? retries : options.retries;
+            maxInFlightRequests = options.maxInFlightRequests
+                ? maxInFlightRequests
                 : options.maxInFlightRequests;
 
             client = new Kafka({
@@ -49,7 +50,7 @@ export const KafkaClient = (function() {
     };
 })();
 
-export const convertCompressionTypes = function(compression) {
+export const convertCompressionTypes = function (compression) {
     switch (compression) {
         case "gzip":
             return CompressionTypes.GZIP;
--- js/kafka/kafka-producer.js
+++ js/kafka/kafka-producer.js
@@ -6,29 +6,31 @@ export class KafkaProducer extends Producer {
     constructor(owner, options = {}) {
         super(owner, options);
 
+        this.metadataMaxAge = conf("KAFKA_PRODUCER_METADATA_MAX_AGE", 300000);
+        this.allowAutoTopicCreation = conf("KAFKA_PRODUCER_AUTO_TOPIC_CREATION", true);
+        this.transactionTimeout = conf("KAFKA_PRODUCER_TRANSITION_TIMEOUT", 60000);
+        this.acks = conf("KAFKA_PRODUCER_ACKS", 0);
+        this.timeout = conf("KAFKA_PRODUCER_TIMEOUT", 30000);
+        this.compression = conf("KAFKA_PRODUCER_COMPRESSION", null);
+
         this.metadataMaxAge =
             options.producerMetadataMaxAge === undefined
-                ? conf("KAFKA_PRODUCER_METADATA_MAX_AGE", 300000)
+                ? this.metadataMaxAge
                 : options.producerMetadataMaxAge;
         this.allowAutoTopicCreation =
             options.producerAutoTopicCreation === undefined
-                ? conf("KAFKA_PRODUCER_AUTO_TOPIC_CREATION", true)
+                ? this.allowAutoTopicCreation
                 : options.producerAutoTopicCreation;
         this.transactionTimeout =
             options.producerTransitionTimeout === undefined
-                ? conf("KAFKA_PRODUCER_TRANSITION_TIMEOUT", 60000)
+                ? this.transactionTimeout
                 : options.producerTransitionTimeout;
-        this.acks =
-            options.producerAcks === undefined
-                ? conf("KAFKA_PRODUCER_ACKS", 0)
-                : options.producerAcks;
+        this.acks = options.producerAcks === undefined ? this.acks : options.producerAcks;
         this.timeout =
-            options.producerTimeout === undefined
-                ? conf("KAFKA_PRODUCER_TIMEOUT", 30000)
-                : options.producerTimeout;
+            options.producerTimeout === undefined ? this.timeout : options.producerTimeout;
         this.compression =
             options.producerCompression === undefined
-                ? conf("KAFKA_PRODUCER_COMPRESSION", null)
+                ? this.compression
                 : options.producerCompression;
 
         const kafkaClient = KafkaClient.getInstance(options);
