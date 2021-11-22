1e24a00dbd421c8b6f6933e5f25a624bc085dd57
Margarida Silva <ms@platforme.com>
2021-11-22 10:48:06 +0000

    test: tests for callback checks of consumer and retry consumer

--- test/api/kafka.js
+++ test/api/kafka.js
@@ -1,3 +1,4 @@
+const fs = require("fs");
 const assert = require("assert");
 const sinon = require("sinon");
 
@@ -61,6 +62,145 @@ describe("Kafka", function() {
             assert.strictEqual(subscribeSpy.calledWithExactly({ topic: "test-topic" }), true);
             assert.strictEqual(runSpy.called, true);
 
+            clientStub.restore();
+        });
+
+        it("should be able to call the callback of a topic bind when receiving messages", async () => {
+            const bus = new api.API({ hosts: "localhost:9092", adapter: "kafka" });
+            const messages = [
+                { value: JSON.stringify({ text: "Test Message 1" }) },
+                { value: JSON.stringify({ text: "Test Message 2" }) }
+            ];
+
+            const fakeConsumer = {
+                connect: () => {},
+                subscribe: () => {},
+                run: ({ eachBatch }) => {
+                    eachBatch({
+                        batch: { topic: "test-topic", messages: messages },
+                        heartbeat: () => {},
+                        isRunning: () => true,
+                        isStale: () => false
+                    });
+                }
+            };
+            const connectSpy = sinon.spy(fakeConsumer, "connect");
+            const subscribeSpy = sinon.spy(fakeConsumer, "subscribe");
+            const runSpy = sinon.spy(fakeConsumer, "run");
+            const fakeKafkaClient = {
+                consumer: () => fakeConsumer
+            };
+            const fakeClient = {
+                client: fakeKafkaClient
+            };
+            const clientStub = sinon.stub(api.KafkaClient, "getInstance");
+            clientStub.returns(fakeClient);
+
+            const callback = sinon.spy();
+
+            await bus.bind("test-topic", {
+                callback: callback
+            });
+
+            assert.strictEqual(connectSpy.called, true);
+            assert.strictEqual(subscribeSpy.calledWithExactly({ topic: "test-topic" }), true);
+            assert.strictEqual(runSpy.called, true);
+            assert.strictEqual(callback.calledTwice, true);
+            assert.strictEqual(
+                callback.firstCall.calledWith({ text: "Test Message 1" }, "test-topic"),
+                true
+            );
+            assert.strictEqual(
+                callback.secondCall.calledWith({ text: "Test Message 2" }, "test-topic"),
+                true
+            );
+
+            clientStub.restore();
+        });
+
+        it("should be able to bind a callback to a topic in the adapter with retry logic", async () => {
+            const bus = new api.API({ hosts: "localhost:9092", adapter: "kafkaRetry" });
+
+            const fakeConsumer = {
+                connect: () => {},
+                subscribe: () => {},
+                run: () => {}
+            };
+            const connectSpy = sinon.spy(fakeConsumer, "connect");
+            const subscribeSpy = sinon.spy(fakeConsumer, "subscribe");
+            const runSpy = sinon.spy(fakeConsumer, "run");
+            const fakeKafkaClient = {
+                consumer: () => fakeConsumer
+            };
+            const fakeClient = {
+                client: fakeKafkaClient
+            };
+            const clientStub = sinon.stub(api.KafkaClient, "getInstance");
+            clientStub.returns(fakeClient);
+
+            await bus.bind("test-topic", {
+                callback: () => {}
+            });
+
+            assert.strictEqual(connectSpy.called, true);
+            assert.strictEqual(subscribeSpy.calledWithExactly({ topic: "test-topic" }), true);
+            assert.strictEqual(runSpy.called, true);
+
+            clientStub.restore();
+        });
+
+        it("should be able to retry to process a message if the bind callback fails", async () => {
+            const bus = new api.API({ hosts: "localhost:9092", adapter: "kafkaRetry" });
+            const messages = [{ value: JSON.stringify({ text: "Test Message 1" }) }];
+
+            const fakeConsumer = {
+                connect: () => {},
+                subscribe: () => {},
+                run: ({ eachBatch }) => {
+                    eachBatch({
+                        batch: { topic: "test-topic", messages: messages },
+                        heartbeat: () => {},
+                        isRunning: () => true,
+                        isStale: () => false
+                    });
+                }
+            };
+            const connectSpy = sinon.spy(fakeConsumer, "connect");
+            const subscribeSpy = sinon.spy(fakeConsumer, "subscribe");
+            const runSpy = sinon.spy(fakeConsumer, "run");
+            const fakeKafkaClient = {
+                consumer: () => fakeConsumer
+            };
+            const fakeClient = {
+                client: fakeKafkaClient
+            };
+            const clientStub = sinon.stub(api.KafkaClient, "getInstance");
+            clientStub.returns(fakeClient);
+
+            const bindOptions = {
+                callback: () => {
+                    throw Error();
+                },
+                retries: 1,
+                retryInterval: 0,
+                retryDelay: 0,
+                messageDelayExponential: 0
+            };
+            const callbackSpy = sinon.spy(bindOptions, "callback");
+
+            await bus.bind("test-topic", bindOptions);
+            await new Promise(resolve => setTimeout(resolve, 0));
+
+            assert.strictEqual(connectSpy.called, true);
+            assert.strictEqual(subscribeSpy.calledWithExactly({ topic: "test-topic" }), true);
+            assert.strictEqual(runSpy.called, true);
+            assert.strictEqual(callbackSpy.callCount, 2);
+            assert.strictEqual(
+                callbackSpy.calledWith({ text: "Test Message 1" }, "test-topic"),
+                true
+            );
+
+            await fs.promises.unlink("data/retry.json");
             clientStub.restore();
         });
     });
